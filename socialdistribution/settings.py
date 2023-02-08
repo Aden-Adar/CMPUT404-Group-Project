50798b9abe6438dcd23407b994a67e606522664f
Aden-Adar <77550086+Aden-Adar@users.noreply.github.com>
2023-02-08 16:38:23 -0700

    trying this instead

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +127,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend', 'build', 'static', 'media')
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
