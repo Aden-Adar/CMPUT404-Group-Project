2a45fa5965fc3e63ccff268688a88b9b5c7a11ad
kdpone <pausangklyde@gmail.com>
2023-02-21 16:47:07 -0700

    settings update

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
+    'base.apps.BaseConfig',
     'rest_framework',
     'rest_framework.authtoken', #https://www.django-rest-framework.org/api-guide/authentication/
 ]
