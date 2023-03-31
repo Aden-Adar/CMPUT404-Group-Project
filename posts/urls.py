61c2afe86b29d7ec04ff1eb83e5af0cc2c4d695f
kdpone <pausangklyde@gmail.com>
2023-03-31 11:54:51 -0600

    image link

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.PostListView.as_view(), name="posts-list"),
     path("<uuid:post_id>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("<uuid:post_id>/image/", views.ImageView.as_view(), name="image-view"),
 ]
