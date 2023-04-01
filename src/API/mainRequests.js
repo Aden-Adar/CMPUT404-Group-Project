15ef5a2e041366836e4055d5d8fb23da5db65589
MG-2003 <mgadhia@ualberta.ca>
2023-03-31 20:04:01 -0600

    Refactor ID calls and post values

--- src/API/mainRequests.js
+++ src/API/mainRequests.js
@@ -1,6 +1,8 @@
-const AUTHOR_ID = window.localStorage.getItem("UUID")
+import { Buffer } from "buffer"
+
+const AUTHOR = JSON.parse(window.localStorage.getItem("Author"))
 const GROUP1URL = "https://social-distribution-w23-t17.herokuapp.com/"
-const GROUP1CREDS = btoa("remote-user-t22:pZHAe3PWukpd3Nv")
+const GROUP1CREDS = Buffer.from("remote-user-t22:pZHAe3PWukpd3Nv").toString('base64')
 async function getExplorePosts() {
     let posts = []
 
@@ -8,10 +10,11 @@ async function getExplorePosts() {
     let authorsResponse = await fetch('/service/authors/')
     let authorsRes_data = await authorsResponse.json()
     for (let i = 0; i < authorsRes_data.items.length; i++) {
-        if (authorsRes_data.items[i].id !== AUTHOR_ID) {
+        if (authorsRes_data.items[i].id !== AUTHOR.id) {
             let authorPosts = []
             let response = await fetch(authorsRes_data.items[i].url + "posts/")
             let res_data = await response.json()
+            console.log("OUR BE->", res_data)
             // Filter out any of logged in users' posts
             for (let j = 0; j < res_data.length; j++) {
                 if (res_data[j].visibility === "PUBLIC") {
@@ -33,7 +36,7 @@ async function getExplorePosts() {
     let authorsRes_data2 = await authorsResponse2.json()
     console.log(authorsRes_data2)
     for (let i = 0; i < authorsRes_data2.items.length; i++) {
-        if (authorsRes_data2.items[i].id !== AUTHOR_ID) {
+        if (authorsRes_data2.items[i].id !== AUTHOR.id) {
             let authorPosts = []
             let response = await fetch(GROUP1URL + 'authors/' + authorsRes_data2.items[i].id + '/posts/', {
                 method: 'GET',
@@ -43,7 +46,7 @@ async function getExplorePosts() {
                 }
             })
             let res_data = await response.json()
-            // Filter out any of logged in users' posts
+            console.log(res_data)
             for (let j = 0; j < res_data.items.length; j++) {
                 if (res_data.items[j].visibility === "PUBLIC") {
                     let commentsResponse = await fetch(GROUP1URL + 'authors/' + authorsRes_data2.items[i].id + '/posts/' + res_data.items[j].id + '/comments/', {
@@ -66,13 +69,13 @@ async function getExplorePosts() {
 }
 
 async function getMyPosts() {
-    let response = await fetch('/service/authors/' + AUTHOR_ID + '/posts/')
+    let response = await fetch(AUTHOR.url + 'posts/')
     let res_data = await response.json()
     return res_data
 }
 
 async function getInbox() {
-    let response = await fetch('/service/authors/' + AUTHOR_ID + '/inbox/')
+    let response = await fetch(AUTHOR.url + 'inbox/')
     let res_data = await response.json()
     return res_data.items
 }
--- src/MainPage/Post.jsx
+++ src/MainPage/Post.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Buffer } from "buffer"
 
 import { Card, CardContent, CardHeader, CardActions, IconButton, Typography, Collapse, List, Divider, ListItem, ListItemText, TextField, ListItemIcon } from '@material-ui/core';
 import { Favorite, Comment, AccountCircle, Send } from '@material-ui/icons';
@@ -6,34 +7,40 @@ import { Favorite, Comment, AccountCircle, Send } from '@material-ui/icons';
 import CommentItem from './Comment';
 
 function PostCard({
-    currentAuthorID,
-    author,
+    postAuthor,
     comments,
     contentType,
     content,
     title,
     visibility,
     published,
-    post_id,
-    url
+    id
 }) {
     const AUTHOR = window.localStorage.getItem("Author")
+    const GROUP1URL = "https://social-distribution-w23-t17.herokuapp.com/"
+    const GROUP1CREDS = Buffer.from("remote-user-t22:pZHAe3PWukpd3Nv").toString('base64')
+    const group1Post = id.includes(GROUP1URL) ? true : false;
+    const internalPost = group1Post ? false : true;
     const [liked, setLiked] = React.useState(false);
     const [comment, setComment] = React.useState('');
     const [commentExpanded, setCommentExpanded] = React.useState(false);
 
     // API CALL TO CHECK IF USER HAS ALREADY LIKED POST
     React.useEffect(() => {
-      async function getLikes() {
-          let likesResponse = await fetch('/service/authors/' + author.id + '/posts/' + post_id + '/likes/')
+      // Call to our BE
+      if (internalPost) {
+        async function getLikes() {
+          let likesResponse = await fetch(id + 'likes/')
           let likesRes_data = await likesResponse.json()
           for (let i = 0; i < likesRes_data.results.length; i++) {
-              if (likesRes_data.results[i].author.id === currentAuthorID) {
+              if (likesRes_data.results[i].author.id === AUTHOR.id) {
                   setLiked(true)
               }
           }
+          }
+          getLikes();
+      } else if (group1Post) {
       }
-      getLikes();
     }, []);
 
     React.useEffect(() => {
@@ -42,111 +49,109 @@ function PostCard({
     // POST LIKE TO INBOX
     function handleLikeClick() {
         if (!liked) {
-          const GROUP1URL = "https://social-distribution-w23-t17.herokuapp.com/"
-          const GROUP1CREDS = btoa("remote-user-t22:pZHAe3PWukpd3Nv")
           let likeBody = {
             "context": "https://www.w3.org/ns/activitystreams",
             "summary": JSON.parse(AUTHOR).displayName + " Likes your post",
             "type": "Like",
             "author": JSON.parse(AUTHOR),
-            "object": url
+            "object": id
           }
           setLiked(true)
-          async function postLike() {
-            let response = await fetch('/service/authors/' + author.id + '/inbox/', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-              },
-              body: JSON.stringify(likeBody)
-            })
-            let res_data = await response.json()
-            console.log(res_data)
+          if (internalPost) {
+            async function postLike() {
+              let response = await fetch(postAuthor.id + 'inbox/', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+                },
+                body: JSON.stringify(likeBody)
+              })
+              let res_data = await response.json()
+            }
+            postLike();
+          } else if (group1Post) {
+            async function postLikeGroup1() {
+              let response = await fetch(GROUP1URL + 'authors/' + postAuthor.id + '/inbox/', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json',
+                  'Authorization': 'Basic ' + GROUP1CREDS,
+                  'Access-Control-Request-Method': 'POST' 
+                },
+                body: JSON.stringify(likeBody)
+              })
+              let res_data = await response.json()
+            }
+            postLikeGroup1();
           }
-          async function postLikeGroup1() {
-            let response = await fetch(GROUP1URL + 'authors/' + author.id + '/inbox/', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': 'Basic ' + GROUP1CREDS,
-                'Access-Control-Request-Method': 'POST' 
-              },
-              body: JSON.stringify(likeBody)
-            })
-            let res_data = await response.json()
-            console.log(res_data)
-          }
-          url.includes(GROUP1URL) ? postLikeGroup1() : postLike();
         }
     }
 
     // POST COMMENT TO POST & INBOX
     function handleSendCommentClick() {
-        const GROUP1URL = "https://social-distribution-w23-t17.herokuapp.com/"
-        const GROUP1CREDS = btoa("remote-user-t22:pZHAe3PWukpd3Nv")
-        async function postComment() {
-          let response = await fetch('/service/authors/' + author.id + '/posts/' + post_id + '/comments/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-            },
-            body: JSON.stringify({ "comment" : comment, "content_type" : "text/plain"})
-          })
-          let res_data = await response.json()
-
-          // POST TO INBOX NOW
-          let inboxRes = await fetch('/service/authors/' + author.id + '/inbox/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-            },
-            body: JSON.stringify(res_data)
-          })
-          let inboxRes_data = await inboxRes.json()
-          console.log(inboxRes_data)
-          window.location.reload(false);
-        }
-
-        async function postCommentGroup1() {
-          let today = new Date()
-          let commentBody = { 
-            "comment" : comment, 
-            "content_type" : "text/plain",
-            "published": today.toISOString(),
-            "type": "comment",
-            "author": JSON.parse(AUTHOR)
+        if (internalPost) {
+          async function postComment() {
+            let response = await fetch(id + 'comments/', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+              body: JSON.stringify({ "comment" : comment, "content_type" : "text/plain"})
+            })
+            let res_data = await response.json()
+  
+            // POST TO INBOX NOW
+            let inboxRes = await fetch(postAuthor.id + 'inbox/', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+              body: JSON.stringify(res_data)
+            })
+            let inboxRes_data = await inboxRes.json()
+            window.location.reload(false);
           }
-          let response = await fetch(GROUP1URL+ 'service/authors/' + author.id + '/posts/' + post_id + '/comments/', {
-            method: 'POST',
-            headers: {
-              'Referer': 'https://social-distribution-w23-t17.herokuapp.com',
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              'Authorization': 'Basic ' + GROUP1CREDS,
-              'Access-Control-Request-Method': 'POST',
-            },
-            body: JSON.stringify(commentBody)
-          })
-          let res_data = await response.json()
-          console.log(res_data)
-          // POST TO INBOX NOW
-          // let inboxRes = await fetch('/service/authors/' + author.id + '/inbox/', {
-          //   method: 'POST',
-          //   headers: {
-          //     'Content-Type': 'application/json',
-          //     'Accept': 'application/json'
-          //   },
-          //   body: JSON.stringify(res_data)
-          // })
-          // let inboxRes_data = await inboxRes.json()
-          // console.log(inboxRes_data)
-          // window.location.reload(false);
+          postComment();
+        } else if (group1Post) {
+          // async function postCommentGroup1() {
+            // let today = new Date()
+            // let commentBody = { 
+            //   "comment" : comment, 
+            //   "content_type" : "text/plain",
+            //   "published": today.toISOString(),
+            //   "type": "comment",
+            //   "author": JSON.parse(AUTHOR)
+            // }
+            // let response = await fetch(GROUP1URL+ 'authors/' + author.id + '/posts/' + url + '/comments/', {
+            //   method: 'POST',
+            //   headers: {
+            //     'Content-Type': 'application/json',
+            //     'Accept': 'application/json',
+            //     'Authorization': 'Basic ' + GROUP1CREDS,
+            //     'Access-Control-Request-Method': 'POST',
+            //   },
+            //   body: JSON.stringify(commentBody)
+            // })
+            // let res_data = await response.json()
+            // console.log(res_data)
+            // POST TO INBOX NOW
+            // let inboxRes = await fetch('/service/authors/' + author.id + '/inbox/', {
+            //   method: 'POST',
+            //   headers: {
+            //     'Content-Type': 'application/json',
+            //     'Accept': 'application/json'
+            //   },
+            //   body: JSON.stringify(res_data)
+            // })
+            // let inboxRes_data = await inboxRes.json()
+            // console.log(inboxRes_data)
+            // window.location.reload(false);
+          // }
         }
-        url.includes(GROUP1URL) ? postCommentGroup1() : postComment();
     }
 
     return (
@@ -155,7 +160,7 @@ function PostCard({
             avatar={<AccountCircle fontSize='large'></AccountCircle>}
             title={
               <span>
-                <Typography component="span" variant="body1">{author.displayName}</Typography><Typography component="span" style={{ color: '#a3a3a3' }}>{"  -  " + title}</Typography>
+                <Typography component="span" variant="body1">{postAuthor.displayName}</Typography><Typography component="span" style={{ color: '#a3a3a3' }}>{"  -  " + title}</Typography>
               </span>}
             subheader={published.substring(0, 10)}
           />
