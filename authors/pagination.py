67204d20a9af9ba6a862cfc81c9d3cb89414008e
Abrar Hussain <77550655+abrarhs@users.noreply.github.com>
2023-04-03 20:43:49 -0600

    image posts

--- authors/pagination.py
+++ authors/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 class CustomPageNumberPagination(pagination.PageNumberPagination):
     ''' Default: 1 page, 5 objects'''
-    page_size = 5
+    page_size = 150
     page_size_query_param = 'count'
     max_page_size = 100000
     page_query_param = 'page'
